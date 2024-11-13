d83a9f6c83ca43f10fd7c63ec4c493b641651bc4
unknown <smddls77@gmail.com>
2024-11-13 10:47:57 +0900

    first commit

---
+++ README.md
@@ -0,0 +1,12 @@
+# DeepClustering
+- This repository is collection of implementation for deep clustering methods
+- Most methods are stemming from Variational Auto-Encoder(VAE) the vanilla VAE
+  - So, even if VAE is not directly developed for the clustering, I implemented the vanilla VAE for comprehension of this field.
+- Some implementations may differ from the original repository because each one is based on my own understanding of the work.
+# List
+- [VAE](https://arxiv.org/abs/1312.6114), ICLR 2014
+- [VaDE](https://arxiv.org/pdf/1611.05148), IJCAI 2017
+
+# To do list
+- [Multi-VAE](https://openaccess.thecvf.com/content/ICCV2021/papers/Xu_Multi-VAE_Learning_Disentangled_View-Common_and_View-Peculiar_Visual_Representations_for_Multi-View_ICCV_2021_paper.pdf), ICCV 2021
+- [Efficient Deep Embedded Subspace Clustering](https://openaccess.thecvf.com/content/CVPR2022/papers/Cai_Efficient_Deep_Embedded_Subspace_Clustering_CVPR_2022_paper.pdf), CVPR 2022
---
+++ vae.py
@@ -0,0 +1,112 @@
+from torch import nn
+import torch
+
+class ConvBlock(nn.Module):
+    def __init__(self, in_channels, out_channels, kernel_size, stride, padding_size):
+        super(ConvBlock, self).__init__()
+        self.conv1 = nn.Conv2d(in_channels, out_channels, kernel_size, stride=stride, padding=padding_size)
+        self.bn1 = nn.BatchNorm2d(out_channels)
+        self.act = nn.SiLU()
+    
+    def forward(self, x):
+        return self.act(self.bn1(self.conv1(x)))
+    
+class ConvBlock_T(nn.Module):
+    def __init__(self, in_channels, out_channels, kernel_size, stride, padding_size):
+        super(ConvBlock_T, self).__init__()
+        self.conv1 = nn.ConvTranspose2d(in_channels, out_channels, kernel_size, stride=stride, padding=padding_size)
+        self.bn1 = nn.BatchNorm2d(out_channels)
+        self.act = nn.SiLU()
+    
+    def forward(self, x):
+        return self.act(self.bn1(self.conv1(x)))
+
+
+class Encoder(nn.Module):
+    def __init__(self, in_channels, base_dim, img_h, img_w, latent_dim=20):
+        super(Encoder, self).__init__()
+        self.latent_dim = latent_dim
+        self.block_1 = ConvBlock(in_channels=in_channels, out_channels=base_dim, kernel_size=3, stride=1, padding_size=1)
+        self.down_1 = ConvBlock(in_channels=base_dim, out_channels=base_dim, kernel_size=4, stride=2, padding_size=1)
+
+        self.block_2 = ConvBlock(in_channels=base_dim , out_channels=base_dim * 2, kernel_size=3, stride=1, padding_size=1)
+        self.down_2 = ConvBlock(in_channels=base_dim * 2, out_channels=base_dim * 2, kernel_size=4, stride=2, padding_size=1)
+
+        self.block_3 = ConvBlock(in_channels=base_dim * 2 , out_channels=base_dim * 4, kernel_size=3, stride=1, padding_size=1)
+        self.down_3 = ConvBlock(in_channels=base_dim * 4, out_channels=base_dim * 4, kernel_size=4, stride=2, padding_size=1)
+
+        self.feat_dim = (img_h // 8) * (img_w // 8) * base_dim * 4
+
+        self.mu_var = nn.Linear(self.feat_dim, latent_dim * 2)
+    
+    def forward(self, x):
+        f_1 = self.down_1(self.block_1(x))
+        f_2 = self.down_2(self.block_2(f_1))
+        f_3 = self.down_3(self.block_3(f_2))
+        mu_var = self.mu_var(f_3.view(-1, self.feat_dim))
+        return mu_var
+    
+
+class Decoder(nn.Module):
+    def __init__(self, latent_dim, out_channels, base_dim, img_h, img_w):
+        super(Decoder, self).__init__()
+        self.img_h, self.img_w = img_h, img_w
+        self.base_dim = base_dim
+        self.project_z = nn.Linear(latent_dim, base_dim)
+
+        self.block_1 = ConvBlock(base_dim, base_dim//2, kernel_size=3, stride=1, padding_size=1)
+        self.up_1 = ConvBlock_T(base_dim // 2, base_dim//2, kernel_size=2, stride=2, padding_size=0)
+
+        self.block_2 = ConvBlock(base_dim // 2, base_dim // 4, kernel_size=3, stride=1, padding_size=1)
+        self.up_2 = ConvBlock_T(base_dim // 4, base_dim // 4, kernel_size=2, stride=2, padding_size=0)
+
+        self.block_3 = ConvBlock(base_dim // 4, base_dim // 8, kernel_size=3, stride=1, padding_size=1)
+        self.up_3 = ConvBlock_T(base_dim // 8, base_dim // 8, kernel_size=2, stride=2, padding_size=0)
+
+        self.final_mu = nn.Conv2d(base_dim // 8, out_channels, kernel_size=3, padding=1)
+        self.final_log_var = nn.Conv2d(base_dim // 8, out_channels, kernel_size=3, padding=1)
+    
+    def forward(self, z):
+        proj_z = self.project_z(z)
+        out_1 = self.block_1(proj_z.view(-1, self.base_dim, self.img_h//8, self.img_w//8))
+        out_1 = self.up_1(out_1)
+
+        out_2 = self.block_2(out_1)
+        out_2 = self.up_2(out_2)
+
+        out_3 = self.block_3(out_2)
+        out_3 = self.up_3(out_3)        
+        return self.final_mu(out_3), self.final_log_var(out_3)
+
+
+class VAE(nn.Module):
+    def __init__(self, in_channels, out_channels, enc_base_dim, dec_base_dim, img_h, img_w, latent_dim=20):
+        super(VAE, self).__init__()
+        self.encoder = Encoder(in_channels, enc_base_dim, img_h, img_w, latent_dim=latent_dim)
+        self.decoder = Decoder(latent_dim=latent_dim, out_channels=out_channels, base_dim=dec_base_dim, img_h=img_h, img_w=img_w)
+    
+    def forward(self, x):
+        mu_var = self.encoder(x)
+        mu, log_var = mu_var[:, :self.encoder.latent_dim], mu_var[:, self.encoder.latent_dim:]
+
+        z = self.reparametrization(mu, log_var)
+        recon_mu, recon_log_var = self.decoder(z)
+        return recon_mu, recon_log_var, mu, log_var
+    
+    def elbo_loss(self, x, mu, log_var, recon_mu, recon_log_var):
+        kl_div = 0.5 * (torch.mat(mu[:, :, None].T, mu[:, :, None]) + torch.exp(log_var).sum(dim=1) - torch.sum(log_var, dim=1) - self.encoder.latent_dim)
+        recon_loss = 0.5 * (recon_log_var + ((recon_mu - x) ** 2) / (torch.exp(recon_log_var)))
+        recon_loss = recon_loss.view(len(x), -1).sum(dim=1)
+        return - kl_div.mean() + recon_loss.mean()
+
+    @torch.no_grad()
+    def generate(self, num_samples):
+        z = torch.randn(num_samples, self.encoder.latent_dim)
+        recon_mu, recon_log_var = self.decoder(z)
+
+        return recon_mu
+    
+    def reparametrization(self, mu, log_var):
+        eps = torch.randn_like(log_var)
+        z = mu + torch.mul(torch.exp(0.5 * log_var), eps)
+        return z
